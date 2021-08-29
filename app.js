6d6bfef1fb0b5ce3db975c96caa95478bed7bf46
Alexandru Cuciureanu <alexandru.cuciureanu@gmail.com>
2021-08-29 11:56:21 +0300

    refactoring

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 require('express-ws')(app);
 const port = 5000;
-const dnsProbe = require('./lib/services/dns/probe.service')
+const dns = require('./lib/services/dns/dns.service');
 
 // TODO Remove when ready.
 // This is used to get some hostnames to the sake of testing
 const blacklist = require('mailchecker').blacklist();
 
-app.ws('/', async (ws) => await dnsProbe.perform(blacklist)((results) => ws.send(JSON.stringify(results))));
+app.ws('/', async (ws) => await dns.probe(blacklist)((results) => ws.send(JSON.stringify(results))));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
